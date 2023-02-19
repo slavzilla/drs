f1117644eb9017d4628809987dc11fdfd39081bd
slavzilla <skovacevic96@outlook.com>
2023-02-19 15:38:44 +0100

    Add solution to first excercise

--- excercise1/src/excercise1/Application.java
+++ excercise1/src/excercise1/Application.java
@@ -3,8 +3,27 @@ package excercise1;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+		Player player1 = new Player();
+		Player player2 = new Player();
+		int count;
+		int total = 0;
+		int iter = 1000;
+		
+		for(int i=0; i<iter; i++)
+		{
+			count = 0;
+			while (true)
+			{
+				player1.throwDice();
+				player2.throwDice();
+				count++;
+				if (player1.getDiceSum() == player2.getDiceSum()) break;
+			}
+			total += count;
+		}
+		
+		System.out.println("Kocke je, u prosjeku, potrebno baciti " 
+		+ (double)total/iter + " puta kako bismo dobili isti zbir.");
 	}
 
 }
