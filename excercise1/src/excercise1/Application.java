360f2db1a4edb2a8b69b94b0002b7e7a2d7b1d71
Slavko Kovacevic <skovacevic96@outlook.com>
2023-02-27 15:27:21 +0100

    Update excercise 1 code to match K & R style

--- excercise1/src/excercise1/Application.java
+++ excercise1/src/excercise1/Application.java
@@ -8,22 +8,21 @@ public class Application {
 		int count;
 		int total = 0;
 		int iter = 1000;
-		
-		for(int i=0; i<iter; i++)
-		{
+
+		for (int i = 0; i < iter; i++) {
 			count = 0;
-			while (true)
-			{
+			while (true) {
 				player1.throwDice();
 				player2.throwDice();
 				count++;
-				if (player1.getDiceSum() == player2.getDiceSum()) break;
+				if (player1.getDiceSum() == player2.getDiceSum())
+					break;
 			}
 			total += count;
 		}
-		
-		System.out.println("Kocke je, u prosjeku, potrebno baciti " 
-		+ (double)total/iter + " puta kako bismo dobili isti zbir.");
+
+		System.out.println("Kocke je, u prosjeku, potrebno baciti " + (double) total / iter
+				+ " puta kako bismo dobili isti zbir.");
 	}
 
 }
--- excercise1/src/excercise1/Player.java
+++ excercise1/src/excercise1/Player.java
@@ -5,26 +5,28 @@ import java.util.Random;
 public class Player {
 	int dice1;
 	int dice2;
-	
+
 	public int getDice1() {
 		return dice1;
 	}
+
 	public void setDice1(int dice1) {
 		this.dice1 = dice1;
 	}
+
 	public int getDice2() {
 		return dice2;
 	}
+
 	public void setDice2(int dice2) {
 		this.dice2 = dice2;
 	}
-	public int getDiceSum()
-	{
+
+	public int getDiceSum() {
 		return dice1 + dice2;
 	}
-	
-	public void throwDice()
-	{
+
+	public void throwDice() {
 		Random rand = new Random();
 		setDice1(1 + rand.nextInt(6));
 		setDice2(1 + rand.nextInt(6));
